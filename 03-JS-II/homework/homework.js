b25b2ddf4532ff7df22d9737c618e38f8e0abf82
Javier <jpereaher@hotmail.com>
2022-06-11 23:02:29 -0500

    Second modification of homework II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,22 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-}
+  if (x < y) {
+    return y;
+    }else if (x > y) {
+     return x;
+    }else (x==y) 
+      return x;
+  }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+    return 'Allowed';
+  } else
+    return 'Not allowed';
 }
   
 function conection(status) {
@@ -19,6 +29,12 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+if (status==1) {
+  return 'Online';
+} else if (status==2) {
+  return 'Away';
+} else 
+return 'Offline';
 }
 
 function saludo(idioma) {
@@ -28,6 +44,14 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma=='aleman') {
+    return 'Guten Tag';
+  } else if (idioma=='mandarin') {
+    return 'Ni Hao';
+  } else if (idioma=='ingles') {
+    return 'Hello';
+  } else 
+  return 'Hola'
 }
 
 function colors(color) {
@@ -38,7 +62,22 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-}
+  //Switch (color) {
+    //case 'blue':
+      //return 'This is blue';
+      //break;
+    //case 'red':
+      //return 'This is red';
+      //break;
+    //case 'green':
+      //return 'This is green';
+      //break;
+    //case 'orange':
+      //return 'This is orange';
+      //break;
+    //defalut:
+      //return 'Color not found'
+  }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
@@ -121,7 +160,7 @@ module.exports = {
   mayoriaDeEdad,
   conection,
   saludo,
-  colors,
+  //colors,
   esDiezOCinco,
   estaEnRango,
   esEntero,
